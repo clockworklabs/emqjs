dc966a3a759e0e733c39d90e1b48fd6b5fd97ca7
Ingvar Stepanyan <me@rreverser.com>
2023-04-23 03:25:14 +0100

    Format options

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 set -e
 cargo rustc --lib --target wasm32-wasi -- -C target-feature=+bulk-memory
-emcc temp.cpp -o temp.js target/wasm32-wasi/debug/libemqjs_runtime.a -s WASM_BIGINT -s ENVIRONMENT=shell -s STANDALONE_WASM -g2 -s ALLOW_MEMORY_GROWTH -sAUTO_{JS,NATIVE}_LIBRARIES=0 -s ASSERTIONS \
-	-s EXPORTED_FUNCTIONS=@exported_funcs.list -s STACK_SIZE=5000000 \
-	-s ERROR_ON_UNDEFINED_SYMBOLS=0 -fexceptions -Wl,--export=EMQJS_VALUE_SPACE,--import-undefined,--export=EMQJS_ENCODED_MODULE,--export=EMQJS_JS,--allow-undefined
+emcc -o temp.js temp.cpp target/wasm32-wasi/debug/libemqjs_runtime.a \
+	-s WASM_BIGINT \
+	-s ENVIRONMENT=shell \
+	-s STANDALONE_WASM \
+	-g2 \
+	-s ALLOW_MEMORY_GROWTH \
+	-sAUTO_{JS,NATIVE}_LIBRARIES=0 \
+	-s ASSERTIONS \
+	-s EXPORTED_FUNCTIONS=@exported_funcs.list \
+	-fexceptions \
+	-s ERROR_ON_UNDEFINED_SYMBOLS=0 \
+	-s STACK_SIZE=1mb \
+	-Wl,--allow-undefined
 cargo run --bin emqjs_preprocess
 wasmtime temp.out.wasm --dir=.
